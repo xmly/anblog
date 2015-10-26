8ceb39fee5811d8a391477696d00a72420534486
gavinyue <gavinyue@amazon.com>
2015-10-26 13:40:03 -0700

    first

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstprojApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstprojApp
+ */
+angular.module('firstprojApp')
+  .controller('MainCtrl', function ($scope,localStorageService) {
+	 var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+ });
